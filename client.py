c27d92404554bbc09ed9748a835b81fff5849b34
Geoffrey Robinson <78464614+Geoff-Robin@users.noreply.github.com>
2024-08-11 22:35:34 +0530

    Added Documentation to client.py

--- client.py
+++ client.py
@@ -2,40 +2,50 @@ import socket
 import threading
 import sys
 
-# Connecting To Server
+# Create a socket object for connecting to the server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server at the specified IP address and port
 client.connect(('127.0.0.1', 55555))
 
 def receive():
+    """Function to handle receiving messages from the server."""
     while True:
         try:
-            # Receive Message From Server
+            # Receive message from server
             message = client.recv(1024).decode('ascii')
+            
+            # Check if the message is an exit command
             if message.startswith("!exit"):
-                username = message.split()
-                print(f"{username[1]} has left!")
-                client.close()
-                sys.exit()
+                username = message.split()  # Split the message to get the username
+                print(f"{username[1]} has left!")  # Inform other clients that this user has left
+                client.close()  # Close the connection to the server
+                sys.exit()  # Exit the program
+
             else:
+                # Print the received message
                 print(message)
+        
         except Exception as e:
+            # Print error message if an exception occurs
             print(f"An error occurred: {e}")
-            client.close()
-            sys.exit()
+            client.close()  # Close the connection to the server
+            sys.exit()  # Exit the program
 
 def write():
+    """Function to handle sending messages to the server."""
     while True:
-        message = input()
+        message = input()  # Get input from the user
         if message == "!exit":
-            client.send(message.encode('ascii'))
-            client.close()
-            sys.exit()
-        client.send(message.encode('ascii'))
+            client.send(message.encode('ascii'))  # Send the exit command to the server
+            client.close()  # Close the connection to the server
+            sys.exit()  # Exit the program
+        client.send(message.encode('ascii'))  # Send the user input to the server
 
-# Start the receiving thread
+# Start a new thread for receiving messages from the server
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 
-# Start the writing thread
+# Start a new thread for sending messages to the server
 write_thread = threading.Thread(target=write)
 write_thread.start()
